73e7a7ea7b7f019bd955dd269dc377aa22119cf9
Danielruizlopezcc <danruilop1@alum.us.es>
2024-11-15 11:01:31 +0100

    Update WSGI module path in settings environment variable

--- joseco/joseco/wsgi.py
+++ joseco/joseco/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joseco.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joseco.joseco.settings')
 
 application = get_wsgi_application()
