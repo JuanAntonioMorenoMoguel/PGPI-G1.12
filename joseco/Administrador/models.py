c4644d0b6bda6a947fb7bb7928e28a074cc087b1
albcarsic <albcarsic@alum.us.es>
2024-11-17 18:25:29 +0100

    Gestión de cursos por admin finalizada
    Co-authored-by: Daniel Ruiz López <Danielruizlopezcc@users.noreply.github.com>

--- joseco/Administrador/models.py
+++ joseco/Administrador/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    # Campo Nombre
+    nombre = models.CharField(max_length=200)
+
+    # Enumerado para Especialidad
+    class EspecialidadChoices(models.TextChoices):
+        JUSTICIA = 'Justicia'
+        SANIDAD = 'Sanidad'
+        EDUCACION = 'Educación'
+        SEGURIDAD = 'Fuerzas y cuerpos de seguridad'
+        ADMIN_GENERAL = 'Administración General'
+        TECNICAS = 'Técnicas y Especialidad'
+        SERV_SOCIALES = 'Servicios Sociales'
+        CULTURALES = 'Culturales'
+        CORREOS = 'Correos'
+        ECONOMIA = 'Economía y Estadística'
+
+    especialidad = models.CharField(
+        max_length=50,
+        choices=EspecialidadChoices.choices,
+    )
+
+    # Enumerado para Modalidad
+    class ModalidadChoices(models.TextChoices):
+        HIBRIDO = 'Híbrido'
+        ONLINE = 'On-Line'
+        PRESENCIAL = 'Presencial'
+
+    modalidad = models.CharField(
+        max_length=20,
+        choices=ModalidadChoices.choices,
+    )
+
+    # Fechas
+    fecha_inicio = models.DateField()
+    fecha_finalizacion = models.DateField()
+
+    # Precio
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        app_label = 'auth'  # Asignar la sección "Autenticación y autorización"
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
