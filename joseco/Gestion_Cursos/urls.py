8c378a6eb602be0ce494b889616c1296541a6897
Enrgaraba <enrgaraba@alum.us.es>
2024-11-18 20:14:22 +0100

    Primeros pasa para creacion de carrito y reservas

--- joseco/Gestion_Cursos/urls.py
+++ joseco/Gestion_Cursos/urls.py
@@ -4,5 +4,8 @@ from Gestion_Cursos import views
 
 urlpatterns = [
     path('cursos/', views.lista_cursos, name='lista_cursos'),
-
+    path('agregar-a-carrito/<int:curso_id>/', views.agregar_a_carrito, name='agregar_a_carrito'),
+    path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path('confirmar-reserva/', views.confirmar_reserva, name='confirmar_reserva'),
+    path('carrito-cantidad/', views.carrito_cantidad, name='carrito_cantidad'),
 ]
--- joseco/Gestion_Cursos/views.py
+++ joseco/Gestion_Cursos/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from Administrador.models import Curso
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from .models import Curso, Carrito
 from .filters import CursoFilter
 
 def lista_cursos(request):
@@ -9,5 +12,42 @@ def lista_cursos(request):
 
     return render(request, 'cursos.html', {'cursos': cursos_filtrados, 'filtro':filtro})
 
+
+
+@login_required
+@csrf_exempt
+def agregar_a_carrito(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    carrito, created = Carrito.objects.get_or_create(usuario=request.user, curso=curso)
+    if not created:
+        carrito.cantidad += 1
+        carrito.save()
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # Comprobar si es AJAX
+        return JsonResponse({"message": "Curso añadido al carrito", "carrito_cantidad": Carrito.objects.filter(usuario=request.user).count()})
     
+    return redirect('ver_carrito')  # Redirección si no es AJAX
+
+@login_required
+def ver_carrito(request):
+    carrito = Carrito.objects.filter(usuario=request.user)
+    return render(request, 'ver_carrito.html', {'carrito': carrito})
+
+@login_required
+def confirmar_reserva(request):
+    carrito = Carrito.objects.filter(usuario=request.user)
+    for item in carrito:
+        if item.curso.vacantes >= item.cantidad:
+            item.curso.vacantes -= item.cantidad
+            item.curso.save()
+        item.delete()
+    return redirect('lista_cursos')
+
+    
+@login_required
+def carrito_cantidad(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        cantidad = Carrito.objects.filter(usuario=request.user).count()
+        return JsonResponse({"carrito_cantidad": cantidad})
+    return JsonResponse({"error": "Acceso no permitido"}, status=403)
 
