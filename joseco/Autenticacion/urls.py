eecc3950f9588b46ba19235b9aa7b387ef3a8959
Juanant <juamormog@gmail.com>
2024-11-18 09:44:43 +0100

    Pagina de contacto hecha

--- joseco/Autenticacion/urls.py
+++ joseco/Autenticacion/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('editar_perfil/', views.editar_perfil, name='editar_perfil'),
     path('', views.cerrar_sesion, name='cerrar_sesion'),
     path('eliminar_cuenta/', views.eliminar_cuenta, name='eliminar_cuenta'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
