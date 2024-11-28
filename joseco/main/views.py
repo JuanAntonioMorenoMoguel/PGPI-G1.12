a1ffd12d7d6c53f75a752f56fc0a622e24af3a7a
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2024-11-28 16:52:49 +0100

    Limitado a 4 cursos

--- joseco/main/views.py
+++ joseco/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from Administrador.models import Curso
 
 def index(request):
-    # Obtengo todos los cursos
-    cursos = Curso.objects.all()
+    # Obtengo 4 cursos
+    cursos = Curso.objects.all()[:4]
     # Paso los cursos a la vista
     return render(request, 'index.html', {'cursos': cursos})
